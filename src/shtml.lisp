988fb0dde322c1bd7d9731b69a35c570c70d7228
t-sin <shinichi.tanaka45@gmail.com>
2019-03-19 10:59:09 +0900

    SHTML allows simple composing with function

--- src/shtml.lisp
+++ src/shtml.lisp
@@ -11,10 +11,10 @@
 (defstruct element
   name props children)
 
-(defun get-name (shtml)
-  (cond ((null shtml) (error "shtml must have its name."))
-        ((not (keywordp (first shtml))) (error "first element of shtml must be a keyword."))
-        (t (first shtml))))
+(defun check-name (name)
+  (if (not (keywordp name))
+      (error "first element of shtml must be a keyword.")
+      name))
 
 (defun get-props (shtml-rest)
   (let ((props))
@@ -32,27 +32,35 @@
                                        (cons k (cons v rest))))))))
     (values props nil)))
 
-(defun make-element* (shtml)
-  (let ((name (get-name shtml)))
-    (multiple-value-bind (props children)
-        (get-props (rest shtml))
-      (make-element :name name
-                    :props props
-                    :children (loop
-                                :for c :in children
-                                :collect (if (listp c) (make-element* c) c))))))
+(defun make-element* (e)
+  (cond ((null e)
+         (error "shtml must have its name."))
+        ((functionp e) e)
+        (t (let ((name (check-name (first e))))
+             (multiple-value-bind (props children)
+                 (get-props (rest e))
+               (make-element :name name
+                             :props props
+                             :children (loop
+                                         :for c :in children
+                                         :collect (if (listp c) (make-element* c) c))))))))
 
 ;; https://developer.mozilla.org/en-US/docs/Glossary/Empty_element
 (defconstant +empty-elements+
   '(:area :base :br :col :embed :hr :img :input :link :meta :param :source :track :wbr))
 
-(defun render-element (e &optional (indent 0))
+(defun render-shtml-element (e params indent)
   (format *standard-output* "~vt<~(~a~)~{~^ ~(~a~)=\"~a\"~}>~%" indent
           (element-name e) (element-props e))
   (loop
     :for c :in (element-children e)
-    :do (if (element-p c)
-            (render-element c (+ indent 2))
-            (format *standard-output* "~vt~a~%" (+ indent 2) c)))
+    :do (render-element c params (+ indent 2)))
   (unless (member (element-name e) +empty-elements+)
     (format *standard-output* "~vt</~(~a~)>~%" indent (element-name e))))
+
+(defun render-element (e params &optional (indent 0))
+  (typecase e
+    (string (format *standard-output* "~vt~a~%" (+ indent 2) e))
+    (element (render-shtml-element e params indent))
+    (function (render-shtml-element (make-element* (funcall e params)) params indent))
+    (t (error "it's not a shtml element: ~s." e))))
