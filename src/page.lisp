e4779aca69c70b0e64fc154d575e72e8aafb70da
t-sin <shinichi.tanaka45@gmail.com>
2019-03-25 00:42:36 +0900

    Page

---
+++ src/page.lisp
@@ -0,0 +1,27 @@
+(defpackage #:asha/page
+  (:use #:cl)
+  (:export))
+(in-package #:asha/page)
+
+(defstruct page
+  title created-at tags body)
+
+(defun created-at (plist)
+  (let ((created-at (getf plist :created-at)))
+    (if created-at  ;; We assumes that `created-at` is valid ISO 8601 string
+        created-at
+        (multiple-value-bind (sec min hour date month year)
+            (get-decoded-time)
+          (format nil "~4,'0d-~2,'0d-~2,'0dT~2,'0d:~2,'0d:~2,'0d"
+                  year month date hour min sec)))))
+
+(defun make-page* (stream)
+  (let ((plist (read stream)))
+    (make-page :title (getf plist :title)
+               :tags (getf plist :tags)
+               :body (getf plist :body)
+               :created-at (created-at plist))))
+
+(defun list-sthml-files (dirname)
+  (uiop:directory-files (make-pathname :directory dirname)
+                        (make-pathname :directory nil :name :wild :type "shtml")))
