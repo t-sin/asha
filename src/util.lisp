90cfdc35ecf554dbfd8193645d280022a42c4f5a
t-sin <shinichi.tanaka45@gmail.com>
2019-03-27 11:20:56 +0900

    Utilize a function for merging pathnames

--- src/util.lisp
+++ src/util.lisp
@@ -1,6 +1,7 @@
 (defpackage #:asha/util
   (:use #:cl)
-  (:export #:now))
+  (:export #:now
+           #:merge-pathnames*))
 (in-package #:asha/util)
 
 (defun now ()
@@ -8,3 +9,9 @@
       (get-decoded-time)
     (format nil "~4,'0d-~2,'0d-~2,'0dT~2,'0d:~2,'0d:~2,'0d"
             year month date hour min sec)))
+
+(defun merge-pathnames* (pathname &rest pathnames)
+  (loop
+    :for p :in pathnames
+    :for merged := (merge-pathnames pathname p) :then (merge-pathnames merged p)
+    :finally (return-from merge-pathnames* merged)))
