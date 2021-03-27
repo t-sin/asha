45b07f7b86b29092e0a3b4894e41fa39be60aef5
t-sin <shinichi.tanaka45@gmail.com>
2021-03-27 23:48:13 +0900

    Simply saving

--- src/asha.lisp
+++ src/asha.lisp
@@ -3,25 +3,14 @@
   (:export))
 (in-package :asha)
 
+;;; website
 (defstruct website
-  rootpath metadata pages templates article-sets)
+  rootpath metadata contents templates article-sets)
 
 (defclass document ()
   ((name :type string
          :initarg :name
          :accessor document-name)))
-
-(defgeneric add-document (document website))
-
-(deftype filetype ()
-  '(member :text :binary))
-
-(defstruct file
-  (path nil :type pathname)
-  (type :text :type filetype)
-  content)
-
-(defgeneric render-document (document website))
 
 (defclass template (document)
   ((params :type symbol
@@ -38,6 +27,47 @@
    (body :initarg :body
          :accessor content-body)))
 
-(defun create-website (rootpath))
+;;; primitive operations
+
+(defgeneric add-document (document website))
+
+(defgeneric dump-document (document))
+
+(deftype filetype ()
+  '(member :text :binary))
+
+(defstruct file
+  (path nil :type pathname)
+  (type :text :type filetype)
+  content)
+
+(defgeneric render-document (document website))
+
+;;; utilities
+
+(defun determine-rootpath (path))
+
+;;; user operations
+
+(defun create-website (rootpath)
+  (make-website :rootpath (truename rootpath)))
+
 (defun load-website (rootpath))
-(defun save-website (website))
+
+(defun save-website (website)
+  (let* ((asha-dir (merge-pathnames (make-pathname :directory '(:relative  ".asha"))
+                                    (website-rootpath website)))
+         (asha-file (merge-pathnames (make-pathname :name "website" :type "lisp") asha-dir))
+         (content-list-file (merge-pathnames (make-pathname :name "contents" :type "lisp") asha-dir))
+         (template-list-file (merge-pathnames (make-pathname :name "templates" :type "lisp") asha-dir)))
+    (ensure-directories-exist asha-dir)
+    (with-open-file (out asha-file :direction :output :if-exists :supersede)
+      (let ((*print-pretty* t))
+        (print website out)))
+    (with-open-file (out content-list-file :direction :output :if-exists :supersede)
+      (print (website-contents website) out))
+    (with-open-file (out template-list-file :direction :output :if-exists :supersede)
+      (print (website-templates website) out))))
+
+(defun add-template (path website))
+(defun add-content (path website))
